4a0706e1734f32bba5d363df743c065499ba7e78
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-16 23:23:46 +0530

    Sakthi has been acting

---
+++ DataStructure_lvl1/Sakthi.c
@@ -0,0 +1,28 @@
+/* Sakthi has been acting strangely for a few days now */
+
+#include <stdio.h>
+#include <math.h>
+int binarySearch(int low,int high,int key){return 0;}
+        int main()
+        {
+            long int n,i,x,root,val,flag=0;
+            scanf("%ld",&n);
+            for(i=1;i<=100000;i++)
+            {
+                val=(i*(i+1))/2;
+                if(val>n/2)
+                break;
+                x=(n-val)*2;
+                root=sqrt(x);
+                if(x==(root*(root+1)))
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==1)
+            printf("YES");
+            else
+            printf("NO");
+            return 0;
+        }
