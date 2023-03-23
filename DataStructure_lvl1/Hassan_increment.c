4d038889ca3b41c742d09fb8b55415bd72170672
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-24 00:10:42 +0530

    The training program period for the first three months is 20000 salary

---
+++ DataStructure_lvl1/Hassan_increment.c
@@ -0,0 +1,44 @@
+/* Hassan gets a job in a software company in Hyderabad. The training program period for the first three months is 20000 salary. Then incremented to 25000 salaries. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isOperator(char x)
+{
+    switch (x) {
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+        return true;
+    }
+    return false;
+}
+string preToPost(string pre_exp)
+{
+    stack<string> s;
+    int length = pre_exp.size();
+    for (int i = length - 1; i >= 0; i--)
+    {
+        if (isOperator(pre_exp[i]))
+        {
+            string op1 = s.top();
+            s.pop();
+            string op2 = s.top();
+            s.pop();
+            string temp = op1 + op2 + pre_exp[i];
+            s.push(temp);
+        }
+        else {
+            s.push(string(1, pre_exp[i]));
+        }
+    }
+    return s.top();
+}
+int main()
+{
+    string pre_exp;
+    cin>>pre_exp;
+    cout<<"Postfix:"<<preToPost(pre_exp);
+    return 0;
+}
