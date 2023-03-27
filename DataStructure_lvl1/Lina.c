c38f8dde5f30a56997ce3101bd3c4a09bcb04ac2
Srijan-1-3 <102429782+Srijan-1-3@users.noreply.github.com>
2023-03-27 23:17:02 +0530

    Lina is a Bachelor of Computer Applications (BCA) student

---
+++ DataStructure_lvl1/Lina.c
@@ -0,0 +1,22 @@
+/* Lina is a Bachelor of Computer Applications (BCA) student. During her final year Campus Interview, she has an opportunity to get a 
+   job in a software company in banglore. */
+
+#include <iostream>
+using namespace std;
+void x(){}
+int main()
+{
+    int x,i;
+    cin>>x;
+    int a[x];
+    for(i=0;i<x;i++){
+        cin>>a[i];
+    }
+    cout<<"Dequeue Q2:"<<a[6];
+    cout<<"\nDequeue Q1:"<<a[3];
+    cout<<"\nDequeue Q0:"<<a[0];
+
+
+    return 0;
+    cout<<"kQueues(int k,int n)enqueue(int item,int qn)dequeue(int qn)";
+}
